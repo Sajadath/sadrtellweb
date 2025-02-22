4def997ff4e390e6c256e4a6b0115582c6a8bd3f
sajad <sajadath1@gmail.com>
2025-02-22 09:51:59 +0330

    navbar fix zindex

--- src/ui/navbar/NavBar.jsx
+++ src/ui/navbar/NavBar.jsx
@@ -3,7 +3,7 @@ import NavbarNavigation from "./NavbarNavigation";
 
 function NavBar() {
   return (
-    <div className="sticky top-0 flex bg-blue-200" dir="rtl">
+    <div className="sticky top-0 z-50 flex bg-blue-200" dir="rtl">
       <Categories />
       <NavbarNavigation />
     </div>
