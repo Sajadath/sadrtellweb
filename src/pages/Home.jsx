f177ad40a55d10e0cf988de9fc2a0d46d8232f7a
sajad <sajadath1@gmail.com>
2025-02-22 17:15:03 +0330

    home chnage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 function Home() {
-  return <div className="h-[1900px] text-center">this is home component</div>;
+  return (
+    <div className="h-[1900px] text-center">
+      <p> this is home component !!</p>
+    </div>
+  );
 }
 
 export default Home;
